83967987313170135a5944119a63b56057f84b7c
Chris Hartke (Uni) <christian.hartke08@gmail.com>
2026-03-16 14:42:02 +0100

    Update energy intensity plotting parameters and add new dimer energy datasets

--- eval_calculations.py
+++ eval_calculations.py
@@ -5,10 +5,10 @@ from chain_coupling import plot_energy_intensity
 import os
 
 
-data_path = "/home/praktikum/Chris/code/data/"
+data_path = "/home/praktikum/Chris/code/data/" #Change according to directory in which the script is being ran
 
 if __name__ == "__main__":
     for entry in os.scandir(data_path):
         if entry.is_file() and entry.name.endswith(".npy"):
             calculations = np.load(entry.  path)
-            plot_energy_intensity(calculations[:, 0], calculations[:, 1], mode="delta", sigma=0.001, header=f"LCMO Spectrum for {entry.name}")
+            plot_energy_intensity(calculations[:, 0], calculations[:, 1], mode="gaussian", sigma=0.005, header=f"LCMO Spectrum for {entry.name}")
--- test.py
+++ test.py
@@ -18,8 +18,16 @@ from ssh_tools import plot_energy
 # ax.plot(helix_points[:, 0], helix_points[:, 1], helix_points[:, 2])
 # plt.show()
 
-# energies = np.array([3.822, 3.999, 4.552, 4.805, 4.986, 4.991, 5.062, 5.086, 5.142, 5.188, 5.288, 5.330, 5.338, 5.369, 5.400, 5.428, 5.470, 5.489, 5.509, 5.555])
-# plot_energy(energies)
+energies_a_dimer = np.array([3.822, 3.999, 4.552, 4.805, 4.986, 4.991, 5.062, 5.086, 5.142, 5.188, 5.288, 5.330]) # 5.338, 5.369, 5.400, 5.428, 5.470, 5.489, 5.509, 5.555])
+energies_c_dimer = np.array([3.798, 3.973, 4.447, 4.609, 4.825, 4.944, 5.030, 5.033, 5.046, 5.070, 5.148, 5.213])
+energies_g_dimer = np.array([3.793, 3.992, 4.429, 4.636, 4.831, 4.989, 5.060, 5.086, 5.224, 5.233, 5.284, 5.319])
+energies_t_dimer = np.array([3.795, 3.977, 4.441, 4.547, 4.916, 4.948, 5.042, 5.053, 5.053, 5.053, 5.053, 5.053])
 
-lcmo_spec = np.load("/home/praktikum/Chris/code/data/testdata/lcmo_spec.npy")
-plot_energy_intensity(lcmo_spec[:, 0], lcmo_spec[:, 1], mode="delta", sigma=0.001)
+if __name__ == "__main__":
+    plot_energy(energies_a_dimer)
+    plot_energy(energies_c_dimer)
+    plot_energy(energies_g_dimer)
+    plot_energy(energies_t_dimer)
+
+# lcmo_spec = np.load("/home/praktikum/Chris/code/data/testdata/lcmo_spec.npy")
+# plot_energy_intensity(lcmo_spec[:, 0], lcmo_spec[:, 1], mode="delta", sigma=0.001)
