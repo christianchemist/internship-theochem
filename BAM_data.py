b2b5e452f17df3f123162261d774973ddc85761d
Chris Hartke (Uni) <christian.hartke08@gmail.com>
2026-03-26 13:04:36 +0100

    Update BAM data handling: add 2x Chromophore and 2x Chromophores no bases; modify energy data for 4x Chromophores; enhance comparison plotting for 2x and 4x Chromophores.

--- BAM_data.py
+++ BAM_data.py
@@ -6,15 +6,18 @@ from ssh_tools import plot_energy, plot_energy_comparison
 from datastructure import data
 
 # "improved" solution for loading all the data, its just not as blocky and a bit more compressed than the previous version. The arrays are stored in a list and the names of the arrays are stored in a separate list, so that they can be easily accessed and plotted. The dataset names are also stored in a separate list for easy access when plotting comparisons.
-array_list = [data["bam"]["full structure"], data["bam"]["short dimer"], data["bam"]["long dimer"], data["bam"]["monomer"], data["bam"]["2x"], data["bam"]["4x"], data["bases monomers"]["adenine"], data["bases monomers"]["cytosine"], data["bases monomers"]["guanine"], data["bases monomers"]["thymine"], data["bam"]["4x no bases"]]
-array_names = ["full structure", "short dimer", "long dimer", "monomer", "2x Chromophore", "4x Chromophore", "adenine monomer", "cytosine monomer", "guanine monomer", "thymine monomer", "4x Chromophores no bases"]
+array_list = [data["bam"]["full structure"], data["bam"]["short dimer"], data["bam"]["long dimer"], data["bam"]["monomer"], data["bam"]["2x"], data["bam"]["4x"], data["bases monomers"]["adenine"], data["bases monomers"]["cytosine"], data["bases monomers"]["guanine"], data["bases monomers"]["thymine"], data["bam"]["2x no bases"], data["bam"]["4x no bases"]]
+array_names = ["full structure", "short dimer", "long dimer", "monomer", "2x Chromophore", "4x Chromophore", "adenine monomer", "cytosine monomer", "guanine monomer", "thymine monomer", "2x Chromophores no bases", "4x Chromophores no bases"]
 dataset_names = ["DFT", "DFTB"]
 fourx = data["bam"]["4x"]
 fourx_no = data["bam"]["4x no bases"]
 comp = np.vstack((fourx[0], fourx_no[0]))
+twox = data["bam"]["2x"]
+twox_no = data["bam"]["2x no bases"]
+comp2 = np.vstack((twox[0], twox_no[0]))
 
 if __name__ == "__main__":
-    mode = input(" DFT/DFTB or comparison? (enter dft or dftb or comparison): ")
+    mode = input(" DFT/DFTB or comparison? (enter dft or dftb or comp): ")
     if mode == "dft":
         for array, name in zip(array_list, array_names):
             plot_energy(array[0], title=f"{name} energies")
@@ -23,8 +26,9 @@ if __name__ == "__main__":
         for array, name in zip(array_list, array_names):
             plot_energy(array[1], title=f"{name} energies with DFTB")
 
-    elif mode == "comparison":
+    elif mode == "comp":
         plot_energy_comparison(comp, list_setnames=["4x Chromophores", "4x Chromophores no bases"], mode="connected", title="4x Chromophore energy comparison plot")
+        plot_energy_comparison(comp2, list_setnames=["2x Chromophores", "2x Chromophores no bases"], mode="connected", title="2x Chromophore energy comparison plot")
         for array, name in zip(array_list, array_names):
             plot_energy_comparison(array, list_setnames=dataset_names, mode="connected", title=f"{name} energy comparison plot")
     else:
