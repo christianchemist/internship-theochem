91d5fd72a50aa527821e43cdea670dace21173e3
Christian Hartke <christian.hartke08@gmail.com>
2026-05-19 10:40:58 +0200

    idk anymore

--- wild.py
+++ wild.py
@@ -1,7 +1,6 @@
 import numpy as np
-from dynamics_data import all_dynamics_plots, single_state_dynamics_plot, single_state_presentation
+from dynamics_data import all_dynamics_plots, single_state_dynamics_plot
 
 full_data = np.load(r"C:\Users\chris\OneDrive\Dokumente\Uni\internship-theochem\data\dynamics\test4\abs_coefficients_all.npy")
 
 all_dynamics_plots(full_data)
-single_state_presentation(full_data)
